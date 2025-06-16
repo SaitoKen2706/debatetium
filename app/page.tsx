f61148eecf61a1488c4c629a76bb8b0b66d98f52
Ken <cuongtruc10@gmail.com>
2025-06-17 02:07:19 +0700

    a

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import TournamentList from "@/components/tournament-list"
-
-export default function Home() {
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <TournamentList />
-    </div>
-  )
-}
